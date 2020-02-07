3ebe08f825c429d19e5767bbafb4d265d6740079
JL-Sandin <46983354+JL-Sandin@users.noreply.github.com>
2020-02-07 14:21:42 -0600

    Add files via upload

---
+++ sketch_feb07a.ino
@@ -0,0 +1,37 @@
+int pm = A0;
+int fs = A7;
+int led = A2;
+long v;
+int fin;
+int fv;
+String sr = " % ";
+String pot = "Val pot ";
+String fot = "Val fot ";
+const int threshold = 500;
+void setup()
+{
+pinMode(pm, INPUT); 
+pinMode(fs, INPUT);
+pinMode(led, OUTPUT);
+Serial.begin(9600);
+} 
+void loop() {
+
+   v = analogRead(pm);
+   
+   int input = analogRead(fs);
+   if (input < v) {
+      digitalWrite(led, HIGH);
+   }
+   else {
+      digitalWrite(led, LOW);
+   }
+   //input 1000 - 0, v = 1023 - 0
+   fin = 100 - (input/10);
+   fv = v*100 / 1023;
+   //Serial.println(input);
+   Serial.println(pot + fv + sr );
+   Serial.println(fot + fin + sr );
+   Serial.println();
+   delay(1000);
+}
